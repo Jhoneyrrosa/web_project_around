6bfa4dd21c9ff1fd7ed591a976409584075118a9
Jhoney Rosa <jhoneyrrosa@gmail.com>
2025-08-11 15:51:46 -0300

    Cards em JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,34 @@
 const modalCloseButton = document.querySelector(".popup__x-popup");
 const popup = document.querySelector(".overlay");
 const modalOpenButton = document.querySelector(".profile__button-edit-name");
+const initialCards = [
+  {
+    name: "Praia de Itamambuca-Ubatuba-SP",
+    image: "https://www.viagenscinematograficas.com.br/wp-content/uploads/2023/01/Ubatuba-Melhores-Praias-Capa.jpg"
+  }
+];
+
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".card");
+const cardsWrapper = document.querySelector(".photo-grid");
+
+function createCard(data){
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".photo-grid__name");
+  const cardImage = cardElement.querySelector(".photo-grid__item");
+console.log(cardElement)
+console.log(cardImage)
+  cardImage.src = data.image;
+  cardImage.alt = data.name;
+
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+
+function renderCard(data, wrap) {
+  wrap.prepend(createCard(data))
+  console.log(wrap)
+}
 
 function closeModal() {
   popup.classList.remove("popup-isOpen");
@@ -32,6 +60,10 @@ function openModal() {
 
 inputName.value = profileName.textContent;
 inputOccupation.value = profileOccupation.textContent;
+
+initialCards.forEach((card) => {
+  renderCard(card, cardsWrapper)
+})
 
 
 
